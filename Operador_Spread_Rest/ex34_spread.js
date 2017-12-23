bf834b36e629fe2deba219ed10eb785979d05d82
Marcos V. R. Camara <marcosramosvc@gmail.com>
2017-12-23 17:33:17 -0300

    Aula de Operador Spread/Rest

---
+++ Operador_Spread_Rest/ex34_spread.js
@@ -0,0 +1,6 @@
+var notasAluno = [6.8, 7.9, 8.2];
+var turmaES5 = notasAluno.concat([6.7, 8.9, 9.1]);
+var turmaES6 = [...notasAluno, 6.7, 8.9, 9.1];
+
+console.log(turmaES5);
+console.log(turmaES6);
